e31988a7016d5c13211111bfb2148c9da01deeb4
jlvelardek <37591354+jlvelardek@users.noreply.github.com>
2021-02-27 22:05:18 -0600

    Update schprice_bot.py

--- schprice_bot.py
+++ schprice_bot.py
@@ -6,10 +6,6 @@ from pycoingecko import CoinGeckoAPI
 import sched, time
 from threading import Timer
 
-
-
-
-
 cg = CoinGeckoAPI()
 #Insert Bot Token
 bot = Bot("XXXXXXXXXXXXXXXXXXXXXX")
@@ -21,7 +17,6 @@ def fetch():
     global counter
     counter+=1
     
-
     #Get SCH ticker from XBTS
     schresponse = requests.get("https://api.xbts.io/api/ticker/sch_sky")
     
@@ -33,30 +28,25 @@ def fetch():
     #Transform call into json / dict
     schresponsejson = schresponse.json()
     print(schresponsejson)
-
-
+        
     #XBTS SCH Metrics
     sch_Price = float(schresponsejson['SCH_SKY']['last'])
     sch_High = str(schresponsejson['SCH_SKY']['high'])
     sch_low = str(schresponsejson['SCH_SKY']['low'])
-
-
+        
     print(sch_Price)
 
     #Math for USD 
     sch_usd = sch_Price * skyprice
-
-
+   
     #Avoid Scnentific Notation
     sch_usd = f'{sch_usd:.10f}'
     sch_Price = f'{sch_Price:.6f}'
 
-    print(sch_usd)
-    print(sch_Price)
-
+    #Coinhour math
+    sch_year = 24*365
 
     #Telegram Bot set-up
-    
     #Insert Bot token
     updater=Updater("XXXXXXXXXXXXXXXXXXX", use_context=True)
     dispatcher=updater.dispatcher
@@ -66,11 +56,6 @@ def fetch():
             chat_id=update.effective_chat.id,
             text= "Exchange: XBTS\n\nTicker: SCH/SKY\nLast price: " +str(sch_Price)+ "\n" + "24h High: "+ sch_High + "\n" + "24h low: " + sch_low +"\n \n"+ "Ticker: SCH/USD " +"\n" "Last price: " + str(sch_usd)
         )
-
-
-
-    sch_year = 24*365
-
 
     #Staking Math
     sky_yearlystaking= float(sch_year) * float(sch_Price) / 100
@@ -90,18 +75,12 @@ def fetch():
     start_value=CommandHandler('skystaking',sky_apy)
     dispatcher.add_handler(start_value)
 
-
     start_value=CommandHandler('schprice', sch_metrics)
     dispatcher.add_handler(start_value)
 
     if counter == 1:
-        updater.start_polling()
-    
-
-    
-    
+        updater.start_polling()    
     
     Timer(5, fetch).start()
     
-
 fetch()
